6e53be9829fa40dcd6659aed3c3714ec9b21d0d5
VictorDemerji <demerji.v1@gmail.com>
2025-10-01 20:10:04 +0300

    Add Student class

---
+++ src/Student.java
@@ -0,0 +1,12 @@
+public class Student {
+    String name;
+    int    age;
+    String groupName;
+    String gender;
+    String hairColor;
+
+    public void lucreaza(String name) {
+        System.out.println("Angajatul " + name + " lucreaza la proiect...");
+
+    }
+}
